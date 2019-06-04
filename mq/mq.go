5eeb8fc414bf09600a15887f57462c65ba5d0691
sorfino <sorfino@andreani.com>
2019-06-04 19:13:40 -0300

    Agrego docs y cambio impl de Listen por channels

--- mq/mq.go
+++ mq/mq.go
@@ -6,22 +6,21 @@ import (
 	"fmt"
 	"errors"
 	"net/http"
+	"time"
 )
 
+//Queue representa el objeto Queue de MQ
 type Queue struct {
 	name string
 	api string
 }
 
-type Listener struct {
-	callback func (data string) 
-	queue Queue
-	context context.Context 
-}
-
+//GetQueue obtiene una cola determinada por la config especificada
 func GetQueue(config Config) Queue {
 	return Queue{config.QueueName,config.HTTPMQAPIUrl}
 }
+
+//Put pone un mensaje en la cola
 func (q Queue) Put(data string) error {
 
 	_,err := resty.R().
@@ -31,31 +30,28 @@ func (q Queue) Put(data string) error {
 
 }
 
+// Listen registra un callback que se invoca cada vez que llega un mensaje a la cola.
 func (q Queue) Listen(ctx context.Context, f func (data string)) {
 	
 	go func() {
-		exit:=false
-
-		go func() {
-			<- ctx.Done()
-			exit = true
-		}()
-
 		url := fmt.Sprintf("%s/queues/%s",q.api,q.name)
+		tick := time.NewTicker(time.Second)
+		defer tick.Stop()
 		for {
-			if exit == true {
-				break
-			}
-			
-			if resp,err := resty.R().Get(url);err == nil && resp.StatusCode() == http.StatusOK {
-				f(resp.String())
+			select {
+			case <-ctx.Done():
+					return
+			case <-tick.C:
+					if resp,err := resty.R().Get(url);err == nil && resp.StatusCode() == http.StatusOK {
+						f(resp.String())
+					}
 			}
 		}
-
 	}()
 		
 }
 
+// Publish publica en el topic 'topic' el mensaje 'data'
 func Publish(topic string, data string) error {
 	return errors.New("not implemented")
 }
