53b77a4b3ec695546a0dfc877a7acd7bdf289ce0
sorfino <sorfino@andreani.com>
2019-06-11 19:23:03 -0300

    refactor callBridge

--- mq/mq.go
+++ mq/mq.go
@@ -2,46 +2,49 @@ package mq
 
 import (
 	"context"
-	"gopkg.in/resty.v1"
 	"fmt"
-	"strings"
 	"net/http"
+	"strings"
 	"time"
+
 	"gopkg.in/eapache/go-resiliency.v1/breaker"
+	"gopkg.in/resty.v1"
 )
 
 //Queue representa el objeto Queue de MQ
 type Queue struct {
 	name string
-	api string
+	api  string
 }
 
+//Topic representa al objeto Topic de MQ
 type Topic struct {
 	name string
-	api string
+	api  string
 }
 
 var circuitBreaker *breaker.Breaker
 
 func init() {
-	circuitBreaker = breaker.New(3,1,5*time.Second)
+	circuitBreaker = breaker.New(3, 1, 5*time.Second)
 }
 
+//GetTopic obtiene un topic para publicar.
 func GetTopic(topic string, config Config) Topic {
-	return Topic{strings.Replace(topic,"/",".",-1),config.HTTPMQAPIUrl}
+	return Topic{strings.Replace(topic, "/", ".", -1), config.HTTPMQAPIUrl}
 }
 
 //GetQueue obtiene una cola determinada por la config especificada
 func GetQueue(config Config) Queue {
-	return Queue{config.QueueName,config.HTTPMQAPIUrl}
+	return Queue{config.QueueName, config.HTTPMQAPIUrl}
 }
 
 //Put pone un mensaje en la cola
 func (q Queue) Put(data string) error {
 
-	url := fmt.Sprintf("%s/queues/%s",q.api,q.name)
+	url := fmt.Sprintf("%s/queues/%s", q.api, q.name)
 	return circuitBreaker.Run(func() error {
-		_,err := resty.R().
+		_, err := resty.R().
 			SetBody(data).
 			Put(url)
 		return err
@@ -50,32 +53,39 @@ func (q Queue) Put(data string) error {
 }
 
 // Listen registra un callback que se invoca cada vez que llega un mensaje a la cola.
-func (q Queue) Listen(ctx context.Context, f func (data string)) {
-	
+func (q Queue) Listen(ctx context.Context, f func(data string)) {
+
 	go func() {
-		url := fmt.Sprintf("%s/queues/%s",q.api,q.name)
-		tick := time.NewTicker(50*time.Millisecond)
-		defer tick.Stop()
+		url := fmt.Sprintf("%s/queues/%s", q.api, q.name)
 		for {
 			select {
 			case <-ctx.Done():
-					return
-			case <-tick.C:
-					if resp,err := resty.R().Get(url);err == nil && resp.StatusCode() == http.StatusOK {
-						f(resp.String())
-					}
+				return
+			default:
+				if resp, err := callMqBridge(url); err == nil {
+					f(resp)
+				}
 			}
 		}
 	}()
-		
+
+}
+
+func callMqBridge(url string) (string, error) {
+	//defer log.Benchmarkf("call resty: ", url)
+	resp, err := resty.R().Get(url)
+	if err == nil && resp.StatusCode() == http.StatusOK {
+		return resp.String(), nil
+	}
+	return "", err
 }
 
 // Publish publica en el topic 'topic' el mensaje 'data'
 func (t Topic) Publish(data string) error {
 
-	url := fmt.Sprintf("%s/topics/%s",t.api,t.name)
+	url := fmt.Sprintf("%s/topics/%s", t.api, t.name)
 	return circuitBreaker.Run(func() error {
-		_,err := resty.R().
+		_, err := resty.R().
 			SetBody(data).
 			Post(url)
 		return err
