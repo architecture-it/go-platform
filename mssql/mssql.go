0dff5842679c22471e778b78629656c6a6cbc8cd
Carolina Fernandez <carolinasolfernandez@gmail.com>
2020-03-06 17:38:01 -0300

    remove benchmark

--- mssql/mssql.go
+++ mssql/mssql.go
@@ -1,27 +1,13 @@
 package mssql
 
 import (
-	"fmt"
 	"os"
-	"time"
 
 	"github.com/eandreani/go-platform/log"
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mssql"
 )
 
 var db *gorm.DB
-
-//Benchmarkf imprime el tiempo que transcurrio en el logger Trace.
-//Ejemplo:
-// defer Benchmarkf("paso el %s","tiempo")
-// imprime: 2019/06/11 17:38:21 log.go:22: paso el tiempo: 1.2121ms
-func Benchmark(fmtt string, args ...string) func() {
-	started := time.Now()
-	return func() {
-		fmt.Printf("%s: %s", fmt.Sprintf(fmtt, args), time.Since(started))
-	}
-}
 
 func init() {
 	dialect := os.Getenv("SQL_DRIVER")
@@ -36,7 +22,7 @@ func init() {
 
 }
 
-// GetDB return the database
+// GetDB return the database connection
 func GetDB() *gorm.DB {
 	return db
 }
