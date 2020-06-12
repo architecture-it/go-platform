b36422160650da4c9aa88ee8d5a5525c1e92319a
“gadielMa” <gadiel.malagrino@gmail.com>
2020-06-12 14:21:10 -0300

    logs nuevos

--- log/log.go
+++ log/log.go
@@ -1,47 +1,89 @@
-package main
+package log
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
+	"path/filepath"
+	"reflect"
+	"runtime"
+	"strconv"
 	"strings"
 	"time"
-
-	"github.com/rs/zerolog"
 )
 
-// log.Info.Println("Info")
-//
+var currentTime = time.Now().Format("2006-01-02 15:04:05.000")
 
-// Benchmark - Imprime el tiempo que transcurrio en el logger Trace.
+var (
+	Trace   = &Logger{Date: currentTime, Level: "-1", Local: os.Args[0], Name: "TRACE", Path: getFileName()}
+	Debug   = &Logger{Date: currentTime, Level: "0", Local: os.Args[0], Name: "DEBUG", Path: getFileName()}
+	Info    = &Logger{Date: currentTime, Level: "1", Local: os.Args[0], Name: "INFO", Path: getFileName()}
+	Warning = &Logger{Date: currentTime, Level: "2", Local: os.Args[0], Name: "WARN", Path: getFileName()}
+	Error   = &Logger{Date: currentTime, Level: "3", Local: os.Args[0], Name: "ERROR", Path: getFileName()}
+	Fatal   = &Logger{Date: currentTime, Level: "4", Local: os.Args[0], Name: "FATAL", Path: getFileName()}
+	Panic   = &Logger{Date: currentTime, Level: "5", Local: os.Args[0], Name: "PANIC", Path: getFileName()}
+)
+
+// Logger estructura a Loggear
+type Logger struct {
+	// Date es la fecha del Log
+	Date string
+
+	// Level cuanto más alta más relevante el Log
+	Level string
+
+	// Local nombre del ejecutable
+	Local string
+
+	// Nombre del Log
+	Name string
+
+	// Path de la ubicación del Log
+	Path string
+
+	// Message principal del Log
+	Message string
+}
+
+// Benchmark imprime el tiempo que transcurrio en el logger Trace.
 // Ejemplo:
 //  defer Benchmarkf("paso el %s","tiempo")
 //  imprime: 2019/06/11 17:38:21 log.go:22: paso el tiempo: 1.2121ms
 func Benchmark(fmtt string, args ...string) func() {
-	//started := time.Now()
+	started := time.Now()
 	return func() {
-		//Trace.Printf("%s: %s", fmt.Sprintf(fmtt, args), time.Since(started))
+		Trace.Pipeline(fmt.Sprintf(fmtt, args) + ": " + string(time.Since(started)))
 	}
 }
 
-func main() {
-	// currentTime := time.Now()
-	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+// Pipeline ..
+func (l *Logger) Pipeline(m string) {
+	var s []string
+	l.Message = m
+	e := reflect.ValueOf(l).Elem()
 
-	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
-	output.FormatLevel = func(i interface{}) string {
-		return strings.ToUpper(fmt.Sprintf("| %-6s|", i))
-	}
-	output.FormatMessage = func(i interface{}) string {
-		return fmt.Sprintf("***%s****", i)
-	}
-	output.FormatFieldName = func(i interface{}) string {
-		return fmt.Sprintf("%s:", i)
-	}
-	output.FormatFieldValue = func(i interface{}) string {
-		return strings.ToUpper(fmt.Sprintf("%s", i))
+	for i := 0; i < e.NumField(); i++ {
+		s = append(s, e.Field(i).String())
 	}
 
-	log := zerolog.New(output).With().Timestamp().Logger()
+	fmt.Println(strings.Join(s, " | "))
+}
 
-	log.Info().Msg("Hello World")
+// JSON ..
+func (l *Logger) JSON(m string) {
+	l.Message = m
+	e, _ := json.Marshal(l)
+	fmt.Println(string(e))
+}
+
+func getFileName() string {
+	_, file, line, ok := runtime.Caller(1)
+	if !ok {
+		file = "???"
+		line = 0
+	} else {
+		file = filepath.Base(file)
+	}
+
+	return file + ":" + strconv.Itoa(line)
 }
